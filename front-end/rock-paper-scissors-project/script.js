7e663c909d2672bf37e9fb66726f9c04515259cf
Jamil Hassiba <jamil.hassiba@student.manchester.ac.uk>
2026-07-12 21:15:50 +0300

    Created playMove function

--- front-end/rock-paper-scissors-project/script.js
+++ front-end/rock-paper-scissors-project/script.js
@@ -1,18 +1,33 @@
-function computerMove() {
+function getComputerMove() {
     const randomNum = Math.random();
-    if (randomNum < 1/3) {
+    if (randomNum < 1/3)
         return 'rock';
-    } else if (randomNum < 2/3) {
+    else if (randomNum < 2/3)
         return 'paper';
-    } else {
+    else
         return 'scissors';
-    }
+}
+
+function playMove(playerMove) {
+    const computerMove = getComputerMove();
+    
+    if (playerMove === computerMove)
+        return 'tie';
+
+    else if (
+        (playerMove === 'paper' && computerMove === 'rock') ||
+        (playerMove === 'rock' && computerMove === 'scissors') ||
+        (playerMove === 'scissors' && computerMove === 'paper')
+    )
+        return 'win';
+    
+    return 'lose';
 }
 
 const emojiBtnElements = document.querySelectorAll('.js-emoji-btn');
 emojiBtnElements.forEach((btnElement) => {
     btnElement.addEventListener('click', () => {
         const playerMove = btnElement.dataset.emoji;
-        console.log(playerMove);
+        playMove(playerMove);
     });
 });
