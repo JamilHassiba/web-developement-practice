dc177ebbc8f9516b5e2b4d3fee977672a2a9cada
Jamil Hassiba <jamil.hassiba@student.manchester.ac.uk>
2026-07-15 20:20:06 +0300

    Products are now added to a cart in data + changed some class namings

--- front-end/amazon-project/scripts/main.js
+++ front-end/amazon-project/scripts/main.js
@@ -1,3 +1,4 @@
+import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML = '';
@@ -9,11 +10,15 @@ products.forEach((product) => {
       </div>
       <div class="product-name">${product.name}</div>
       <div class="product-rating-container">
-        <img class="product-stars-rating" src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <img class="product-stars-rating" 
+        src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count">${product.rating.count}</div>
       </div>
-      <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
-      <select class="product-count-selector">
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2)}
+      </div>
+      <select class="product-quantity-selector 
+      js-product-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -25,9 +30,44 @@ products.forEach((product) => {
         <option value="9">9</option>
         <option value="10">10</option>
       </select>
-      <button class="add-to-cart-button">Add to Cart</button>
+      <button class="add-to-cart-button js-add-to-cart-button" 
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
     </div>
 `});
 
-console.log(productsHTML);
 document.querySelector('.products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart-button')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      // Get product id
+      const id = button.dataset.productId;
+
+      // Get selected quantity
+      const quantity = Number(
+        document.querySelector(`.js-product-quantity-selector-${id}`).value
+      );
+      
+      // Check if product is already in cart
+      let matchingProduct;
+      cart.forEach((product) => {
+        if (product.id === id) {
+          matchingProduct = product;
+        }
+      });
+
+      // Increase product quantity if already in cart
+      if (matchingProduct) {
+        matchingProduct.quantity += quantity;
+      }
+      // Add to cart otherwise
+      else {
+        cart.push({
+          id,
+          quantity
+        });
+      }
+    });
+  });
